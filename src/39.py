3ee769dddc4155f57fbcce0711463dbb9ce95b6b
restojohn547 <restojohn547@gmail.com>
2025-05-19 11:52:27 +0000

    Add 39.py

---
+++ src/39.py
@@ -0,0 +1,13 @@
+def sum_of_squares(a, b):
+    """
+    Calculate the sum of squares of two numbers
+
+    Parameters:
+    a (int): The first number
+    b (int): The second number
+
+    Returns:
+    int: The sum of squares of the two numbers
+    """
+
+    return a ** 2 + b ** 2
